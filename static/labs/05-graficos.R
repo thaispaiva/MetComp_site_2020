009742f5a72e424e13043d0d7657f882e7e7b195
thaispaiva <thaisvip@gmail.com>
2021-01-04 20:28:53 -0300

    1st class week #4

--- static/labs/05-graficos.R
+++ static/labs/05-graficos.R
@@ -1,20 +1,33 @@
 ## EST171 - Métodos Computacionais para Análise de Risco
 ## Código da aula 05 - Gráficos no R
 
+
+
+
 ## Dados de ciclones tropicais nos EUA de 1899-2006
+
 StormMax = read.table("../datasets/extremedatasince1899.csv", header=T, sep=",")
 attach(StormMax)
 
+
 table( trunc(Yr/10)*10 )[-1]
 
+
 # gráfico de barra do número de ciclones por década
+
 barplot( table( trunc(Yr/10)*10 )[-1] )
 
+
 # gráfico de linhas - vel. média por década e região
+
 decade = trunc(Yr/10)*10
 interaction.plot(decade, Region, Wmax, type="b", pch=1:5)
 
+
+
+
 ## Graficos lado a lado
+
 
 set.seed(123)
 x = rnorm(100)    # gerando amostra
@@ -50,27 +63,43 @@ lines(u, pnorm(u,mean(x),sd(x)), lty=2)   # f.d. normal
 # adicionar legenda
 legend("topleft", c("F.D. Empírica", "F.D. Normal", "F.D. Kernel"),
        col=c("grey","black","black"), lwd=c(2,1,1), lty=c(1,2,1), bty="n")
+
+
+
+
+
 ## Graficos para classes de objetos
 ## Voltando aos dados de ciclones...
+
 
 # selecionando ciclones que ocorreram após 1977 na região 'Basin'
 StormMax.Basin = subset(StormMax, (Region=="Basin")&(Yr>1977))
 attach(StormMax.Basin)
 
+
 # boxplot da velocidade por ano
+
 boxplot(Wmax ~ as.factor(Yr), xlab="Ano", ylab="Velocidade (nós)", col="grey")
+
+
+
 
 # regressão linear entre velocidade do vento e ano
 model = lm(Wmax ~ Yr)
 par(mfrow=c(2,2))
 plot(model)
 
+
 ## Graficos 3D
 
 # exemplo normal bivariada
+
 binorm = function(x1,x2,r=0){
   exp( -(x1^2 + x2^2 - 2*r*x1*x2)/( 2*(1-r^2) ) )/( 2*pi*sqrt(1-r^2) )
 }
+
+
+
 
 x = y = seq(-2.5, 2.5, by=.25)
 z = outer(x, y, function(u,v) binorm(u,v,r=.4))
@@ -88,5 +117,6 @@ contour(x, y, z, add=TRUE)
 
 # grafico 2: superfície 3D
 persp(x, y, z)
+
 
 persp(x, y, z, theta=210, col=gray.col[45], shade=TRUE)
